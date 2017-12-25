2eb491bda36d2e08e4be9d4b6762a2d80ec8dd06
Ivan Smurygin <ivsm0613@netcracker.com>
2017-12-25 17:26:28 +0300

    Collection tasks are finished

---
+++ src/com/smuryginim/kotlin/stepik_course/collections/Partition_3_9.kt
@@ -0,0 +1,8 @@
+package com.smuryginim.kotlin.stepik_course.collections
+
+/*
+    val numbers = listOf(1, 3, -4, 2, -11)
+    val (positive, negative) = numbers.partition { it > 0 }
+    positive == listOf(1, 3, 2)
+    negative == listOf(-4, -11)
+ */
---
+++ src/com/smuryginim/kotlin/stepik_course/properties/Properties_4_1.kt
@@ -0,0 +1,6 @@
+package com.smuryginim.kotlin.stepik_course.properties
+
+/**
+ * @author ivsm0613
+ * @date 25.12.2017
+ */
